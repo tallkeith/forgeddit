aba407a812b526d00ee75034d0b3e413dde27c40
tallkeith <tallestkeith@gmail.com>
2016-04-02 15:27:01 -0400

    added some validations to post and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
 	has_many :posts
 	has_many :comments
+
+	validates_presence_of :password, :email, :username
+  validates :password, length: { minimum: 5 }
+  validates :email, format: {
+              with: /.+@.+\..+/,  message: "Must have an @ symbol and a period."
+            }
 end
