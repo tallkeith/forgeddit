ccdce0d7283acff9eb051b8794033ea73c259bb1
tallkeith <tallestkeith@gmail.com>
2016-04-02 15:25:58 -0400

    created create method in logins controller

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,1 +1,23 @@
-logins_controller.rb
+class LoginsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by(email: params["email"],
+                         password: params["password"])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "Logged in as #{@user.username}."
+      redirect_to :root
+    else
+      flash[:notice] = "Could not find matching username and password."
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to :root
+  end
+end
